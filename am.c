783d46a7a56a559f037b02b72f312b074c5ff18f
samariclara-ux <samariclarance@gmail.com>
2026-02-25 05:57:28 +0100

    DIGITAL CLOCK!
    creates a digital clock showing the real time. Hour, minutes and seconds.

---
+++ am.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdbool.h>
+#include <windows.h>
+
+// DIGITAL CLOCK
+
+int main() {
+  time_t rawtime =0;
+  struct tm *pTime = NULL;
+  bool isRunning = true;
+
+  printf("DIGITAL CLOCK\n");
+
+  while (isRunning) {
+    time(&rawtime);
+    pTime = localtime(&rawtime);
+
+    printf("\r%02d:%02d:%02d", pTime->tm_hour, pTime->tm_min, pTime->tm_sec);
+   
+    Sleep(1000);
+   
+  }
+  return 0; 
+
+}
